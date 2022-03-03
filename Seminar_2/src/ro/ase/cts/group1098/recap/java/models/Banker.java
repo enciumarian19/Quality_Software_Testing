5ba7083ca9207452d925543688ff82b30fc95cc7
RobrutaChan <enciumarian19@stud.ase.ro>
2022-03-03 21:17:08 +0200

    Final commit :))

---
+++ Seminar_2/src/ro/ase/cts/group1098/recap/java/models/Banker.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.group1098.recap.java.models;
+
+import ro.ase.cts.group1098.recap.java.exceptions.IllegalTransferException;
+
+public class Banker {
+
+	private int nextId = 1;
+	private String bankName;
+	
+	public Banker(String bankName) {
+		super();
+		this.bankName = bankName;
+	}
+	
+	public BankAccount openAccount(AccountType type) throws IllegalTransferException {
+		String iban = bankName + nextId;
+		this.nextId += 1;
+		switch (type) {
+		case CURRENT:
+			return new CurrentAccount(iban);
+		case SAVINGS:
+			return new SavingsAccount(iban);
+		default:
+			throw new UnsupportedOperationException();
+		}
+	}
+}
